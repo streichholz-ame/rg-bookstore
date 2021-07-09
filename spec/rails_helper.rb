e147abe8c82f978807b6114ba0ac95750f573d87
streichholz-ame <ovcharenko.nastia@gmail.com>
2021-07-09 14:16:59 +0300

    add configs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ SimpleCov.start do
 end
 
 require File.expand_path('../config/environment', __dir__)
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'capybara'
 require 'capybara/rails'
@@ -32,5 +32,5 @@ RSpec.configure do |config|
 
   config.filter_rails_from_backtrace!
 
-  config.include(Shoulda::Matchers::ActionController, { type: :model, file_path: /spec\/controllers/})
+  config.include(Shoulda::Matchers::ActionController, { type: :model, file_path: %r{spec/controllers} })
 end
