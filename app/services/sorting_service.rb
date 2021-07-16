5b26d590e6535d8b9afa4c8643172a7ad9fa8a2c
streichholz-ame <ovcharenko.nastia@gmail.com>
2021-07-16 11:45:06 +0300

    modify sorting service

--- app/services/sorting_service.rb
+++ app/services/sorting_service.rb
@@ -3,11 +3,11 @@ class SortingService
   attr_reader :books, :params
 
   SORTING_METHODS = {
-    :newest => 'id DESC',
-    :title_asc => 'name ASC',
-    :title_desc => 'name DESC',
-    :price_asc => 'price ASC',
-    :price_desc => 'price DESC'
+    newest: 'id DESC',
+    title_asc: 'name ASC',
+    title_desc: 'name DESC',
+    price_asc: 'price ASC',
+    price_desc: 'price DESC'
   }
 
   def initialize(books, params)
@@ -17,15 +17,16 @@ class SortingService
 
   def call
     @books_by_category = show_books_by_categories
-    sort_books(by)
+    sort_books
   end
 
-  def sort_books(by)
-    @books_by_category.order(SORTING_METHODS.values_at(by))
+  def sort_books
+    params[:sort] = :newest unless SORTING_METHODS.include?(params[:sort]&.to_sym)
+    
+    @books_by_category.order(SORTING_METHODS.key(params[:sort]))
   end
 
   def show_books_by_categories
-    category = Book.where(category_id: params[:category_id])
-    category.presence || books
+    params[:id].nil? ? books : books.where(category_id: params[:id])
   end
 end
