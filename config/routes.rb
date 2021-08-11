e19bf3e4ff96a918cefcc5be16e99212cf51ba96
streichholz-ame <ovcharenko.nastia@gmail.com>
2021-08-11 12:30:40 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
 
   devise_for :users,
              controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
   get '/users', to: redirect('users/sign_up')
 
   resources :catalog, controller: 'catalog', only: :index
