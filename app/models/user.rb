78423e8a36b535644fa4c8e503f907799a2ca12a
streichholz-ame <ovcharenko.nastia@gmail.com>
2021-08-11 13:14:42 +0300

    facebook issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   has_one :shipping_address, as: :addressable, dependent: :destroy
 
   def self.from_omniauth(auth)
-    binding.pry
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
