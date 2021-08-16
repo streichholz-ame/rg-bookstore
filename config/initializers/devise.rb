0f4c0f948da24d4bcc08704cbc209a59e654aeff
streichholz-ame <ovcharenko.nastia@gmail.com>
2021-08-16 14:33:30 +0300

    credentials changes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,7 +275,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
 
-  config.omniauth :facebook, Rails.application.credentials.facebook[:id], Rails.application.credentials.facebook[:secret]
+  config.omniauth :facebook, Rails.application.credentials[:facebook_id], Rails.application.credentials[:facebook_secret]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.credentials.facebook[:id], Rails.application.credentials.facebook[:secret]
+  provider :facebook, Rails.application.credentials[:facebook_id], Rails.application.credentials[:facebook_secret]
 end
