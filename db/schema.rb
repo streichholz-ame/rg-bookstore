827449223b47574651f575a1c38041d1cd36366b
streichholz-ame <ovcharenko.nastia@gmail.com>
2021-08-20 13:47:56 +0300

    resolve conflict in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_132754) do
+ActiveRecord::Schema.define(version: 2021_08_19_113533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,23 @@ ActiveRecord::Schema.define(version: 2021_08_18_132754) do
     t.index ["book_id"], name: "index_images_on_book_id"
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "book_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_order_items_on_book_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.text "review_text"
@@ -138,4 +155,7 @@ ActiveRecord::Schema.define(version: 2021_08_18_132754) do
 
   add_foreign_key "books", "categories"
   add_foreign_key "images", "books"
+  add_foreign_key "order_items", "books"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "users"
 end
