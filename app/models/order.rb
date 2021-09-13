fe27633806366b0168a0bfa2708fe34023884f5b
streichholz-ame <ovcharenko.nastia@gmail.com>
2021-09-13 10:43:58 +0300

    add models

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,11 @@ class Order < ApplicationRecord
   include AASM
   belongs_to :user, optional: true
   belongs_to :coupon, optional: true
+  belongs_to :delivery, optional: true
+  belongs_to :credit_card, optional: true
+  has_one :address
 
+  scope :checkout_process, -> { where(status: %w[cart address delivery payment confirm]) }
   scope :processing, -> { where(status: 'complete') }
   scope :in_delivery, -> { where(status: 'in_delivery') }
   scope :delivered, -> { where(status: 'delivered') }
@@ -10,6 +14,47 @@ class Order < ApplicationRecord
 
   has_many :order_items, dependent: :destroy
 
-  enum status: { cart: 0, log_in: 1, address: 2, shipping: 3, payment: 4, confirm: 5, complete: 6,
-                 in_delivery: 7, delivered: 8, canceled: 9 }
+  aasm(:status) do
+    state :cart
+    state :address_type
+    state :delivery_type
+    state :payment
+    state :confirm
+    state :complete
+    state :in_delivery
+    state :delivered
+    state :canceled
+
+    event :address_type do
+      transitions from: :cart, to: :address_type
+    end
+
+    event :delivery_type do
+      transitions from: :address_type, to: :delivery_type
+    end
+
+    event :payment do
+      transitions from: :delivery_type, to: :payment
+    end
+
+    event :confirm do
+      transitions from: :payment, to: :confirm
+    end
+
+    event :complete do
+      transitions from: :confirm, to: :complete
+    end
+
+    event :in_delivery do
+      transitions from: :complete, to: :in_delivery
+    end
+
+    event :delivered do
+      transitions from: :in_delivery, to: :delivered
+    end
+
+    event :canceled do
+      transitions from: %i[complete in_delivery delivered], to: :canceled
+    end
+  end
 end
