b95e3a69219df211bebd416509085d9cc0a8fed7
streichholz-ame <ovcharenko.nastia@gmail.com>
2021-10-05 13:20:33 +0300

    delete fog spec

--- spec/support/fog_helper.rb
+++
@@ -1,8 +0,0 @@
-Fog.mock!
-Fog.credentials_path = Rails.root.join('config/fog_credentials.yml')
-connection = Fog::Storage.new(
-  provider: 'AWS',
-  aws_access_key_id: Rails.application.credentials[:S3_KEY],
-  aws_secret_access_key: Rails.application.credentials[:S3_SECRET]
-)
-connection.directories.create(key: 'dev-bucket')
