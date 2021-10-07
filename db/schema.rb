1c3ef9dfece02803680df277d97508e20e8d42c0
streichholz-ame <ovcharenko.nastia@gmail.com>
2021-10-07 11:23:51 +0300

    small fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_06_113256) do
+ActiveRecord::Schema.define(version: 2021_09_16_114649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 2021_10_06_113256) do
 
   create_table "coupons", force: :cascade do |t|
     t.string "number"
-    t.decimal "discount", precision: 2, default: "4"
+    t.decimal "discount", precision: 2, default: "8"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
